4a52a4eef3bcfbfb123e238047d9f9584e3fcd69
Dopamine <Khoa Nguyen>
2025-03-14 17:55:34 +0700

    Commit

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -35,13 +35,13 @@ export const SearchInput = () => {
 
   return (
     <div className="relative">
-      <Search className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
+      {/* <Search className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
       <Input
         onChange={(e) => setValue(e.target.value)}
         value={value}
         className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200 dark:bg-slate-900"
         placeholder="Search for a course"
-      />
+      /> */}
     </div>
   );
 };
