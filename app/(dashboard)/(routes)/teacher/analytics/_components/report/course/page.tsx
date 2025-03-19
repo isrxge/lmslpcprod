66068a15497e85abdd06dc4f059a332f15af8cc5
Dopamine <Khoa Nguyen>
2025-03-19 09:35:34 +0700

    Commit

--- app/(dashboard)/(routes)/teacher/analytics/_components/report/course/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/_components/report/course/page.tsx
@@ -1,3 +1,159 @@
+// "use client";
+
+// import { useQuery } from "react-query";
+
+// import { DataTable } from "./_components/data-table";
+// import { columns } from "./_components/columns";
+// import axios from "axios";
+// import { auth, useAuth } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
+// import { useState, useEffect } from "react";
+
+// const ReportPageCourse = () => {
+//   const { isLoaded, isSignedIn, userId, sessionId, getToken } = useAuth();
+//   const [hasAdvanceReportPermission, setHasAdvanceReportPermission] =
+//     useState(false);
+//   const [departmentId, setDepartmentId] = useState<string | null>(null);
+//   if (!userId) {
+//     return redirect("/");
+//   }
+
+//   const fetchUserPermission = async () => {
+//     try {
+//       const { data } = await axios.get(`/api/user/${userId}/personalInfo`);
+//       const advanceReportPermission = data.userPermission.some(
+//         (permission: { permission: { title: string } }) =>
+//           permission.permission.title === "Advance report permission"
+//       );
+//       setHasAdvanceReportPermission(advanceReportPermission);
+
+//       return data;
+//     } catch (error) {
+//       console.error("Error fetching user permissions:", error);
+//     }
+//   };
+  
+
+//   const fetchAllCourses = async () => {
+//     try {
+//       const { data } = await axios.get(`/api/courses`);
+
+//       if (hasAdvanceReportPermission) {
+//         // If the user has "Advance report permission", return all courses
+//         return data;
+//       } else {
+//         // If the user does not have "Advance report permission", filter courses by departmentId
+//         const filteredCourses: any = [];
+//         for (let i = 0; i < data.length; i++) {
+         
+//           let indexOf = data[i].CourseOnDepartment.find(
+//             (item: any) => item.departmentId == data1.departmentId
+//           );
+
+//           if (indexOf != null) {
+//             filteredCourses.push(data[i]);
+//           }
+//         }
+        
+//         console.log("Filtered courses by department:", filteredCourses);
+//         return filteredCourses;
+//       }
+//     } catch (error) {
+//       console.error("Error fetching courses:", error);
+//       throw error; // Propagate error for react-query to handle
+//     }
+//   };
+//   const { data: data1, error: error1, isLoading: isLoading1 } = useQuery("userinfo", fetchUserPermission, {
+//     refetchOnWindowFocus: false,
+//   });
+  
+  
+  
+//   const { data, error, isLoading } = useQuery(
+//     ["allCourse", data1],
+//     fetchAllCourses,
+//     {
+//       enabled: !!data1, // This ensures the second query only runs when the first one is successful
+//       refetchOnWindowFocus: false,
+//     }
+//   );
+//   // const fetchAllCourses = async () => {
+//   //   try {
+//   //     const { data } = await axios.get(`/api/courses`);
+//   //     console.log("Fetched all courses:", data);
+
+//   //     if (hasAdvanceReportPermission) {
+//   //       // If the user has "Advance report permission", return all courses
+//   //       return data;
+//   //     } else {
+//   //       // If the user doesn't have the permission, filter courses by departmentId
+//   //       const filteredCourses = data.filter((course: any) => {
+//   //         console.log("Course's CourseOnDepartment:", course.CourseOnDepartment.some((courseDepartment: any) =>
+//   //           courseDepartment.departmentId === departmentId // Compare with the departmentId of the logged-in user
+//   //         )); // Log CourseOnDepartment array
+
+//   //         // Check if the departmentId matches any in the CourseOnDepartment array
+//   //         return course.CourseOnDepartment.some((courseDepartment: any) =>
+//   //           courseDepartment.departmentId === departmentId // Compare with the departmentId of the logged-in user
+//   //         );
+//   //       });
+//   //       console.log("Filtered courses by department:", filteredCourses);
+//   //       return filteredCourses;
+//   //     }
+//   //   } catch (error) {
+//   //     console.error("Error fetching courses:", error);
+//   //     throw error; // Propagate error for react-query to handle
+//   //   }
+//   // };
+
+//   // const fetchAllCourses = async () => {
+//   //   try {
+//   //     const { data } = await axios.get(`/api/courses`);
+//   //     console.log("Fetched all courses:", data);
+
+//   //     if (hasAdvanceReportPermission) {
+//   //       // If the user has "Advance report permission", return all courses
+//   //       return data;
+//   //     } else {
+//   //       // If the user doesn't have the permission, filter courses by departmentId
+//   //       const filteredCourses = data.filter((course: any) => {
+//   //         console.log("Course's CourseOnDepartment:", course.CourseOnDepartment); // Log CourseOnDepartment array
+
+//   //         // Use filter to find the departmentId in the CourseOnDepartment array
+//   //         const matchingDepartments = course.CourseOnDepartment.filter((courseDepartment: any) =>
+//   //           courseDepartment.departmentId === departmentId // Compare with the departmentId of the logged-in user
+//   //         );
+
+//   //         // Log the matching departments for debugging
+//   //         console.log("Matching departments:", matchingDepartments);
+
+//   //         // Return the course only if matching departments exist
+//   //         return matchingDepartments.length > 0;
+//   //       });
+//   //       console.log("Filtered courses by department:", filteredCourses);
+//   //       return filteredCourses;
+//   //     }
+//   //   } catch (error) {
+//   //     console.error("Error fetching courses:", error);
+//   //     throw error; // Propagate error for react-query to handle
+//   //   }
+//   // };
+
+  
+//     if (isLoading) {
+//       return <></>;
+//     } else {
+//       return (
+//         <div className="p-6">
+//           <DataTable columns={columns} data={data} />
+//         </div>
+//       );
+//     }
+  
+// };
+
+// export default ReportPageCourse;
+
 "use client";
 
 import { useQuery } from "react-query";
@@ -5,148 +161,26 @@ import { useQuery } from "react-query";
 import { DataTable } from "./_components/data-table";
 import { columns } from "./_components/columns";
 import axios from "axios";
-import { auth, useAuth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import { useState, useEffect } from "react";
 
 const ReportPageCourse = () => {
-  const { isLoaded, isSignedIn, userId, sessionId, getToken } = useAuth();
-  const [hasAdvanceReportPermission, setHasAdvanceReportPermission] =
-    useState(false);
-  const [departmentId, setDepartmentId] = useState<string | null>(null);
-  if (!userId) {
-    return redirect("/");
-  }
-
-  const fetchUserPermission = async () => {
-    try {
-      const { data } = await axios.get(`/api/user/${userId}/personalInfo`);
-      const advanceReportPermission = data.userPermission.some(
-        (permission: { permission: { title: string } }) =>
-          permission.permission.title === "Advance report permission"
-      );
-      setHasAdvanceReportPermission(advanceReportPermission);
-
-      return data;
-    } catch (error) {
-      console.error("Error fetching user permissions:", error);
-    }
-  };
-  const {
-    data: data1,
-    error: error1,
-    isLoading: isLoading1,
-  } = useQuery("userinfo", fetchUserPermission, {
-    refetchOnWindowFocus: false,
-  });
-
   const fetchAllCourses = async () => {
-    try {
-      const { data } = await axios.get(`/api/courses`);
+    const { data } = await axios.get(`/api/courses`);
 
-      if (hasAdvanceReportPermission) {
-        // If the user has "Advance report permission", return all courses
-        return data;
-      } else {
-        // If the user does not have "Advance report permission", filter courses by departmentId
-        const filteredCourses: any = [];
-        for (let i = 0; i < data.length; i++) {
-         
-          let indexOf = data[i].CourseOnDepartment.find(
-            (item: any) => item.departmentId == data1.departmentId
-          );
-
-          if (indexOf != null) {
-            filteredCourses.push(data[i]);
-          }
-        }
-        
-        console.log("Filtered courses by department:", filteredCourses);
-        return filteredCourses;
-      }
-    } catch (error) {
-      console.error("Error fetching courses:", error);
-      throw error; // Propagate error for react-query to handle
-    }
+    return data;
   };
 
-  const { data, error, isLoading } = useQuery(["allCourse", data1], fetchAllCourses, {
+  const { data, error, isLoading } = useQuery("allCourse", fetchAllCourses, {
     refetchOnWindowFocus: false,
   });
-  // const fetchAllCourses = async () => {
-  //   try {
-  //     const { data } = await axios.get(`/api/courses`);
-  //     console.log("Fetched all courses:", data);
 
-  //     if (hasAdvanceReportPermission) {
-  //       // If the user has "Advance report permission", return all courses
-  //       return data;
-  //     } else {
-  //       // If the user doesn't have the permission, filter courses by departmentId
-  //       const filteredCourses = data.filter((course: any) => {
-  //         console.log("Course's CourseOnDepartment:", course.CourseOnDepartment.some((courseDepartment: any) =>
-  //           courseDepartment.departmentId === departmentId // Compare with the departmentId of the logged-in user
-  //         )); // Log CourseOnDepartment array
-
-  //         // Check if the departmentId matches any in the CourseOnDepartment array
-  //         return course.CourseOnDepartment.some((courseDepartment: any) =>
-  //           courseDepartment.departmentId === departmentId // Compare with the departmentId of the logged-in user
-  //         );
-  //       });
-  //       console.log("Filtered courses by department:", filteredCourses);
-  //       return filteredCourses;
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching courses:", error);
-  //     throw error; // Propagate error for react-query to handle
-  //   }
-  // };
-
-  // const fetchAllCourses = async () => {
-  //   try {
-  //     const { data } = await axios.get(`/api/courses`);
-  //     console.log("Fetched all courses:", data);
-
-  //     if (hasAdvanceReportPermission) {
-  //       // If the user has "Advance report permission", return all courses
-  //       return data;
-  //     } else {
-  //       // If the user doesn't have the permission, filter courses by departmentId
-  //       const filteredCourses = data.filter((course: any) => {
-  //         console.log("Course's CourseOnDepartment:", course.CourseOnDepartment); // Log CourseOnDepartment array
-
-  //         // Use filter to find the departmentId in the CourseOnDepartment array
-  //         const matchingDepartments = course.CourseOnDepartment.filter((courseDepartment: any) =>
-  //           courseDepartment.departmentId === departmentId // Compare with the departmentId of the logged-in user
-  //         );
-
-  //         // Log the matching departments for debugging
-  //         console.log("Matching departments:", matchingDepartments);
-
-  //         // Return the course only if matching departments exist
-  //         return matchingDepartments.length > 0;
-  //       });
-  //       console.log("Filtered courses by department:", filteredCourses);
-  //       return filteredCourses;
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching courses:", error);
-  //     throw error; // Propagate error for react-query to handle
-  //   }
-  // };
-
-  if (isLoading1) {
+  if (isLoading) {
     return <></>;
   } else {
-    if (isLoading) {
-      return <></>;
-    } else {
-      return (
-        <div className="p-6">
-          <DataTable columns={columns} data={data} />
-        </div>
-      );
-    }
+    return (
+      <div className="p-6">
+        <DataTable columns={columns} data={data} />
+      </div>
+    );
   }
 };
 
